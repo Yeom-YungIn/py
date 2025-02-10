93c021328ea909cde3fab87a17d058915eff29b3
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-02-10 23:48:36 +0900

    chore: operator course

---
+++ operator_course.py
@@ -0,0 +1,29 @@
+print(1+1)
+print(3-1)
+print(5*2)
+print(6/3)
+print(7/3)
+
+print(2**3)
+print(5%3)
+
+print(7//3)
+
+print(10 > 3)
+
+print (10//3 > 4//2)
+print (10//3 < 4//2)
+print(1 >= 5)
+
+print(True == 1)
+print(False == 1)
+print(False == 0)
+print(1 != 3)
+
+print ((10 > 3) and (5 > 2))
+print ((10 > 3) & (5 > 2))
+print ((10 > 3) | (5 > 2))
+
+
+print(not True > -2)
+print(-2)
