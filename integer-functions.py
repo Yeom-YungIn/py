2c3d4f691a839a5db2b63b8ee7def3f1b0487564
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-02-11 22:53:44 +0900

    chore: 숫차 처리 함수

---
+++ integer-functions.py
@@ -0,0 +1,12 @@
+print(abs(-5)) # 절대값 5
+print(pow(4,2)) # 4^2
+print(max([1,2,3,4,5,6,7]))
+print(min([1,2,3,4,5,6,7]))
+print(max(1,6,7,8))
+print(min(1,2,7,8))
+print(round(3.14195))
+
+from math import *
+print(floor(3.14195))
+print(ceil(3.14195))
+print(sqrt(16))
