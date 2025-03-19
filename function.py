fe5bb2c639c8af6271e2109dc4a19ac9b6384015
Yeom-YungIn <80555282+Yeom-YungIn@users.noreply.github.com>
2025-03-19 23:14:43 +0900

    chore: python function param/return

--- function.py
+++ function.py
@@ -2,3 +2,21 @@ def open_account():
     print("opening account function!!")
 
 open_account()
+
+balance = 0
+
+def deposit(money):
+    global balance
+    balance = balance + money
+    return balance
+
+deposit(100)
+print(balance)
+
+def aaa(a):
+    a += 1
+    print(a)
+    return a
+
+b = aaa
+b(10)
